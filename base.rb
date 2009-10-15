90aa1253a00bdce4219117a9b7a411c6b78fccb4
Ola Karlsson <olkarls@gmail.com>
2009-10-15 17:13:21 +0200

    Duplicate code removed

--- base.rb
+++ base.rb
@@ -10,7 +10,6 @@ rake "gems:install GEM=chriseppstein-compass", :sudo => true
 
 gem "chriseppstein-compass-960-plugin", :source => "http://gems.github.com", :lib => "ninesixty"
 rake "gems:install GEM=chriseppstein-compass-960-plugin", :sudo => true
-css_framework = "960"
 plugin_require = "-r ninesixty"
 
 compass_command = "compass --rails -f #{css_framework} . --css-dir=#{css_dir} --sass-dir=#{sass_dir} "
@@ -32,7 +31,6 @@ run "rm 'public/javascripts/prototype.js'"
 
 run "echo TODO: Write documentation > README"
 
-gem 'haml'
 gem 'searchlogic'
 gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
 gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com'
@@ -41,7 +39,9 @@ gem 'validatious-on-rails', :source => 'http://gemcutter.org'
 
 rake "gems:install", :sudo => true
 
-plugin "custom-err-msg", :git => "git://github.com/gumayunov/custom-err-msg.git"
+plugin "custom-err-msg",        :git => "git://github.com/gumayunov/custom-err-msg.git"
+plugin "has_permalink",         :git => "git://github.com/olkarls/has_permalink.git"
+plugin "generic_controllers",   :git => "git://github.com/olkarls/generic_controllers.git"
 
 generate :formtastic
 generate :validatious
